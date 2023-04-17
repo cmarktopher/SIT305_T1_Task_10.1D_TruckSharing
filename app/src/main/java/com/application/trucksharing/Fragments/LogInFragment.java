cf9df73582649249910dca9a9909717cebb33367
chris-marktopher <cmarktopher@gmail.com>
2023-04-17 17:44:29 +1000

    Sign up page UI done and transition added

--- app/src/main/java/com/application/trucksharing/Fragments/LogInFragment.java
+++ app/src/main/java/com/application/trucksharing/Fragments/LogInFragment.java
@@ -1,14 +1,17 @@
 package com.application.trucksharing.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import com.application.trucksharing.R;
+import com.application.trucksharing.databinding.FragmentLogInBinding;
 
 /**
  * Fragment for the log in page - this will also be the entry view the user sees when opening the app
@@ -35,9 +38,24 @@ public class LogInFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+        // Create our binding and view
+        FragmentLogInBinding binding = FragmentLogInBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        // Bind to sign up button so that we can transition to sign up fragment which has the sign up form
+        binding.signUpButton.setOnClickListener(onClickView -> {
+
+            FragmentManager fragmentManager = ((AppCompatActivity) requireContext()).getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .setCustomAnimations(R.anim.transition_in, R.anim.transition_out, R.anim.transition_in, R.anim.transition_out)
+                    .setReorderingAllowed(true)
+                    .replace(R.id.coreFragmentContainer, SignUpFragment.newInstance(), null)
+                    .commit();
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/application/trucksharing/Fragments/SignUpFragment.java
@@ -0,0 +1,42 @@
+package com.application.trucksharing.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.application.trucksharing.R;
+
+/**
+ * Fragment for the sign up form
+ */
+public class SignUpFragment extends Fragment {
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+    public static SignUpFragment newInstance() {
+        SignUpFragment fragment = new SignUpFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+}
